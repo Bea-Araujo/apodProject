2277325d29358840fdefd48cf864cfcc48db06be
Bea-Araujo <beatriz.a.0702@gmail.com>
2022-04-11 15:48:07 -0300

    adicionado meio para conferir se é uma data válida; no caso de não ser, é retornado um texto em vermelho

--- scripts/index.js
+++ scripts/index.js
@@ -2,39 +2,53 @@
 let date = '2022-01-11';
 const apiKey = 'JpvYcrSZt9TwWowd9NfaXf8jFml7WqTVRY9G2UIg';
 
+let currentDate = new Date();
+const day = currentDate.getDate().toString().padStart(2, '0');
+const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+const year = currentDate.getFullYear().toString();
+
+currentDate = `${year}-${month}-${day}`;
+
 $("#submit-btn").click(function (event) {
     event.preventDefault();
     // $("#root").css('background-color', 'blue')
     const choosenDate = $("#dateSetter").val();
-    console.log(choosenDate)
+    if (choosenDate > currentDate) {
+        $('#api-response').html('');
+        const invalidDate = document.createElement('p');
+        $(invalidDate).text('Data inválida!').css('color', 'red');
+        $('#api-response').append(invalidDate);
+        $(invalidDate).fadeOut(3000);
+    } else {
+        $.ajax({
+            url: `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${choosenDate}`,
+            success: function (response) {
+                $('#api-response').html('');
 
-    $.ajax({
-        url: `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${choosenDate}`,
-        success: function (response) {
-            $('#api-response').html('');
+                const title = document.createElement('h2');
+                $(title).text(`${response.title}`)
+                $('#api-response').append(title)
 
-            const title = document.createElement('h2');
-            $(title).text(`${response.title}`)
-            $('#api-response').append(title)
+                if (response.media_type == 'image') {
+                    const picture = document.createElement('picture')
+                    const img = document.createElement('img');
+                    $(img).attr('src', `${response.url}`)
+                    $(picture).append(img)
+                    $('#api-response').append(picture)
+                } else if (response.media_type == 'video') {
+                    const iframe = document.createElement('iframe');
+                    $(iframe).attr('src', `${response.url}`);
+                    $('#api-response').append(iframe);
+                }
 
-            if (response.media_type == 'image') {
-                const picture = document.createElement('picture')
-                const img = document.createElement('img');
-                $(img).attr('src', `${response.url}`)
-                $(picture).append(img)
-                $('#api-response').append(picture)
-            } else if (response.media_type == 'video') {
-                const iframe = document.createElement('iframe');
-                $(iframe).attr('src', `${response.url}`);
-                $('#api-response').append(iframe);
+                const description = document.createElement('p');
+                $(description).text(`${response.explanation}`)
+                $('#api-response').append(description)
+                console.log(response);
             }
+        })
+    }
 
-            const description = document.createElement('p');
-            $(description).text(`${response.explanation}`)
-            $('#api-response').append(description)
-            console.log(response);
-        }
-    })
 });
 
 
