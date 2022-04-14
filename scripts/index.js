2913d43f8339f56b0c8ed5a5592d0c45f6923b97
Bea-Araujo <beatriz.a.0702@gmail.com>
2022-04-14 00:46:19 -0300

    foi adicionada a div loading, na função success foi adicionado timer para esperar antes de mostrar o card, foi adicionado um botão para fechar o card, foi adicionada uma função para fechar ser feito o fechamento do card

--- scripts/index.js
+++ scripts/index.js
@@ -12,44 +12,99 @@ currentDate = `${year}-${month}-${day}`;
 $("#submit-btn").click(function (event) {
     event.preventDefault();
     $('#api-response').html('');
-    $('.direita').css('visibility', 'visible');
+    $('.direita').css('visibility', 'hidden');
 
     // $("#root").css('background-color', 'blue')
     const choosenDate = $("#dateSetter").val();
     if (choosenDate > currentDate) {
+        $('#api-response').css('visibility', 'visible');
+        $('#api-response').css('opacity', '100%')
         const invalidDate = document.createElement('p');
         $(invalidDate).text('Data inválida!').css('color', 'red');
         $('#api-response').append(invalidDate);
-        $(invalidDate).fadeOut(3000);
+        let opacity = 100
+
+        setTimeout(() => {
+            setInterval(() => {
+                if (opacity < 0) return
+                console.log(opacity)
+                $('#api-response').css('opacity', `${opacity}%`)
+                opacity--
+            }, 30);
+        }, 1000);
+
     } else {
         $.ajax({
             url: `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${choosenDate}`,
             success: function (response) {
+                $('#loading').css('display', 'block');
+                // $('#api-response').css('visibility', 'hiddden');
+
                 const title = document.createElement('h2');
                 $(title).text(`${response.title}`)
-                $('#api-response').append(title)
+
 
                 const div = document.createElement('div');
                 if (response.media_type == 'image') {
                     const picture = document.createElement('picture')
                     const img = document.createElement('img');
-                    $(img).attr('src', `${response.url}`);
+                    $(img).attr('src', `${response.hdurl}`);
                     $(picture).append(img);
                     $(div).append(picture);
                     // $('#api-response').append(picture)
                 } else if (response.media_type == 'video') {
                     const iframe = document.createElement('iframe');
-                    $(iframe).attr('src', `${response.url}`);
+                    $(iframe).attr('src', `${response.hdurl}`);
                     $(div).append(iframe);
                     // $('#api-response').append(iframe);
                 }
 
                 const description = document.createElement('p');
                 $(description).text(`${response.explanation}`)
-                // $('#api-response').append(description)
+
                 $(div).append(description);
-                $('#api-response').append(div);
+
+
                 console.log(response);
+                const closeBtn = document.createElement('button');
+                closeBtn.id = 'closeBtn'
+                closeBtn.innerText = 'X'
+                closeBtn.classList.add('closeBtn')
+                closeBtn.addEventListener('click', close);
+
+                //$(closeBtn).attr('id', 'closeBtn')
+                // $("#closeBtn").addClass('closeBtn')
+                // $("#closeBtn").attr('onclick', close())
+
+
+
+                // $('#api-response').fadeIn(3000)
+                let opacity = 0;
+                console.log('a')
+                setTimeout(function () {
+
+                    setInterval(() => {
+
+                        if (opacity == 0) {
+                            $('#api-response').append(title);
+                            $('#api-response').append(div);
+                            $('#api-response').append(closeBtn);
+                            opacity++
+                        }
+                        else if (opacity <= 100) {
+
+                            $('#loading').css('display', `none`)
+                            $('#api-response').css('visibility', 'visible');
+                            $('#api-response').css('opacity', `${opacity}%`)
+                            opacity++
+                        } else {
+                            return
+                        }
+                    }, 30);
+                    console.log('b')
+                }, 900)
+                console.log('c')
+
             }
         })
     }
@@ -57,4 +112,22 @@ $("#submit-btn").click(function (event) {
 });
 
 
+function close() {
+    // const closeBtn = document.querySelector('#closeBtn');
+    // closeBtn.style.color = 'red';
+    fadeCard()
+}
+
+function fadeCard() {
+    let opacity = 100;
+    setTimeout(() => {
+        setInterval(() => {
+            if (opacity < 0) return;
+            else if (opacity == 0) $('#api-response').html('');
+            console.log(opacity);
+            $('#api-response').css('opacity', `${opacity}%`);
+            opacity--
+        }, 20);
+    }, 1000);
+}
 
